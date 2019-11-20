19bebc51fb9f82378768f0382243304eba6924a7
giovdigua <giovdigua@gmail.com>
2019-11-20 19:00:29 +0100

    End of jsnack 9

--- 9/main.js
+++ 9/main.js
@@ -1,1 +1,8 @@
+//Calcola la somma e la media dei primi 10 numeri
+var somma = 0;
+for (var i = 1; i <=10; i++) {
 
+    somma+= i;
+}
+
+console.log('La somma dei primi 10 numeri è ' + somma + ' la loro media è ' + (somma/10));
